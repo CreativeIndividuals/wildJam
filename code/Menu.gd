11bc1726e547829b8d3fbd9bd75665fcc10c0ad8
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-06-22 17:59:43 +0530

    almost done ig

--- code/Menu.gd
+++ code/Menu.gd
@@ -4,7 +4,12 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Game.tscn") # Replace with function body.
 
 func _on_quit_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()
+	
+
+func _on_htp_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Howtoplay.tscn") 
+ # Replace with function body.
 	
 func _ready() -> void:
 	$ost.play()
--- code/gameOver.gd
+++ code/gameOver.gd
@@ -13,7 +13,7 @@ func _on_quit_pressed():
 
 
 func _on_game_state_game_over(correct: bool) -> void:
-	result_label.text = "Congrats You Have " + ("Won" if correct else "Lost")
+	result_label.text = "You Have " + ("Won" if correct else "Lost") + "The Game"
 	result_details.text = "it was your "+%GameState.impostor_character.character_name+" all along"
 	show()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
